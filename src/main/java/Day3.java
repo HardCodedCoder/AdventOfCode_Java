2ed614b7c3209a268f2fa6e2cd7c556c1f24769b
ginovalentinopensuni <gino.pensuni@gmail.com>
2023-02-03 17:28:07 +0100

    Finished implementation of Day 3.

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -1,6 +1,8 @@
-import java.io.File;
+package main.java;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class Day3
 {
@@ -21,10 +23,10 @@ public class Day3
     public static void main(String[] args)
     {
         FileReader reader = new FileReader();
-        List<String> rucksacks;
+        List<String> stringRucksacks = null;
         try
         {
-            rucksacks = reader.readFile("src/main/resources/day3_input.txt");
+            stringRucksacks = reader.readFile("src/main/resources/day3_input.txt");
         }
         catch (IllegalArgumentException | IOException e)
         {
@@ -32,6 +34,31 @@ public class Day3
             System.exit(1);
         }
 
+        List<Rucksack> rucksacks = Rucksack.convertToRucksacks(stringRucksacks);
 
+        System.out.format("Priority of all double items: %d", Rucksack.totalItemPriorityCounter);
+        System.out.format("The sum of all priorities of item types: %d", calculateItemPriorityOfAllGroups(rucksacks));
+    }
+
+    private static int calculateItemPriorityOfAllGroups(List<Rucksack> rucksacks)
+    {
+        int counter = 0;
+        for (int groupIdx = 0; groupIdx <= rucksacks.size()- 3; groupIdx += 3)
+        {
+            for (Character character : rucksacks.get(groupIdx).getAllElements())
+            {
+                if (!rucksacks.get(groupIdx + 1).contains(character))
+                    continue;
+
+                if (!rucksacks.get(groupIdx + 2).contains(character))
+                    continue;
+
+                 counter += Rucksack.calculateItemPriority(character);
+
+                 break;
+            }
+        }
+
+        return counter;
     }
 }
--- src/main/java/Rucksack.java
+++ src/main/java/Rucksack.java
@@ -1,27 +1,53 @@
+package main.java;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.*;
 
 /**
  * This class represents a Rucksack containing
  * two compartments.
  */
-public class Rucksack
-{
+public class Rucksack {
+    public static int totalItemPriorityCounter = 0;
+    /**
+     * The offset needed to full range of 'A' to 'Z' value from 27 to 52.
+     */
+    private static final int capitalLetterOffset = 38;
+
+    /**
+     * The offset needed to full range of 'a' to 'z' value from 1 to 26.
+     */
+    private static final int lowerLetterOffset = 96;
+
     /**
      * Using a HashSet for better lookup perfomance O(1).
      * Holds the first part of the elements in the rucksack.
      */
     private List<Character> compartmentOne;
 
+
     /**
      * Holds the second part of the elements in the rucksack.
      */
     private List<Character> compartmentTwo;
 
-    public Rucksack(String content)
-    {
+    private String compartmentTwoAsString;
+
+    private int itemPriority;
+
+    public static List<Rucksack> convertToRucksacks(List<String> lines) {
+        List<Rucksack> rucksacks = new ArrayList<>();
+
+        for (String line : lines)
+        {
+            rucksacks.add(new Rucksack(line));
+        }
+
+        return rucksacks;
+    }
+
+    public Rucksack(String content) {
         if (content == null || content.isEmpty())
             throw new IllegalArgumentException("The content of the rucksack mustn't be null or empty.");
 
@@ -29,15 +55,44 @@ public class Rucksack
         this.compartmentTwo = new ArrayList<>();
 
         fillCompartments(content);
+        Character character = getDoubleElement();
+
+        this.itemPriority = calculateItemPriority(character);
+
+        Rucksack.totalItemPriorityCounter += this.itemPriority;
+    }
+
+    public static int calculateItemPriority(Character item)
+    {
+        if (Character.isUpperCase(item))
+            return (int)item - Rucksack.capitalLetterOffset;
+        else
+            return (int)item - Rucksack.lowerLetterOffset;
+    }
+
+    public List<Character> getAllElements()
+    {
+        List<Character> result = new ArrayList<>();
+        result.addAll(this.compartmentOne);
+        result.addAll(this.compartmentTwo);
+        return result;
+    }
+
+    public boolean contains(Character character)
+    {
+        return this.compartmentOne.contains(character) || this.compartmentTwo.contains(character);
     }
 
     /**
      * Fills the compartments of the Rucksack
+     *
      * @param content The content divided in the middle.
      */
     private void fillCompartments(String content) {
         String firstCompartment = content.substring(0, content.length() / 2);
-        String secondCompartment = content.substring(content.length() / 2, content.length() - 1);
+        String secondCompartment = content.substring(content.length() / 2, content.length());
+
+        this.compartmentTwoAsString = secondCompartment;
 
         this.compartmentOne = firstCompartment
                 .chars()
@@ -46,9 +101,23 @@ public class Rucksack
 
         this.compartmentTwo = secondCompartment
                 .chars()
-                .mapToObj(e -> (char)e)
+                .mapToObj(e -> (char) e)
                 .collect(Collectors.toList());
     }
 
+    private Character getDoubleElement()
+    {
+        for (Character character : this.compartmentOne)
+        {
+            int index = compartmentTwoAsString.indexOf(character);
 
+            if (index != - 1)
+            {
+                return character;
+            }
+        }
+
+        throw new IllegalStateException("There is no double element in both compartments");
+    }
 }
+
