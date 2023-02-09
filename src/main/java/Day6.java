af0ea667a8cfff7aff703a6eb95ad3d0c06823c9
ginovalentinopensuni <gino.pensuni@gmail.com>
2023-02-09 20:53:38 +0100

    Implemented Day 6

---
+++ src/main/java/Day6.java
@@ -0,0 +1,48 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Day6
+{
+    public static void main(String [] main)
+    {
+        FileReader reader = new FileReader();
+        List<String> puzzleInput = new ArrayList<>();
+        try
+        {
+            puzzleInput = reader.readFile("src/main/resources/day6_input.txt");
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+        String communicationStream = puzzleInput.get(0);
+        int startOfPacketMarkerIndex = indexOfPacketMarker(communicationStream);
+        System.out.println(startOfPacketMarkerIndex);
+    }
+
+    private static int indexOfPacketMarker(String communicationStream)
+    {
+        int result = -1;
+        int endIdx = 14;
+        for (int idx = 0; idx < communicationStream.length(); idx++)
+        {
+            String substring = communicationStream.substring(idx, endIdx);
+            HashSet<Character> tmp = new HashSet<>();
+
+            for (char character : substring.toCharArray())
+                tmp.add(character);
+
+            if (tmp.size() == 14)
+                return endIdx;
+            else
+                endIdx++;
+        }
+
+        return -1;
+    }
+}
